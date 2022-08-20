e3b92f0301fee9dad377e5377d62f89798ac0e45
sedykhaleks <106698556+sedykhaleks@users.noreply.github.com>
2022-08-20 12:42:30 +0300

    Update lab_2.pas

--- lab_2.pas
+++ lab_2.pas
@@ -33,3 +33,30 @@ begin
 	writeln('Наибольшее расстояние между точками ', n1, ' и ', n2);
         readln;
 end.
+
+
+uses crt;
+var  
+  i, k : integer;
+  digits : set of byte;
+  flag : boolean;
+begin
+  clrscr;
+  for i := 10 to 32767 do
+    begin
+      flag := true;
+	  digits := [];
+	  k := i;
+      while (flag) and (k > 0) do
+	    if  not((k mod 10) in digits) 
+		 then 
+		  begin 
+		    digits := digits + [k mod 10];
+			k := k div 10;
+		  end
+         else flag := false;
+      if flag then write(i, ' ');
+    end;
+  readln;
+end.
+
